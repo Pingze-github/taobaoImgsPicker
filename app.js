d2f8f0e0350210f2b5681bfb04b24a9cb8d7d2dc
wangshunzhang <wang719695@gmail.com>
2018-01-17 23:47:45 +0800

    调整前端

--- app.js
+++ app.js
@@ -11,7 +11,10 @@ app.use(express.static('public'));
 
 app.get('/pick', async (req, res) => {
   const text = req.query.key;
+  const start = Date.now();
+  console.log('开始解析', new Date().toLocaleString());
   const imgs = await imgsPicker(text);
+  console.log('完成解析，耗时', Date.now() - start, new Date().toLocaleString());
   if (!imgs) return res.send({Code: 1, Msg: '解析失败', Data:{}})
   res.send({Code: 0, Msg: '解析成功', Data:{imgs}})
 });
